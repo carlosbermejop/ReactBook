becf77cfd8f73b9849b773e2e52a0a9157b1ad69
Carlos Bermejo Pérez <carlos.bermejo@nexthink.com>
2020-12-05 11:18:34 +0100

    Book init

---
+++ JavaScript_exercises.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch");
+
+const getFakePerson = async () => {
+    try {
+        let res = await fetch("https://api.randomuser.me/?nat=US&results=1");
+        let { results } = await res.json();
+        console.log(results);
+    } catch (error) {
+        console.error(error);
+    }
+    
+};
+
+getFakePerson();
